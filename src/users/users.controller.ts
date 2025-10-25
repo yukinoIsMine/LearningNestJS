7cb77ffc755418529a953567262002c5a30433ac
yukinoIsMine <dayrotisthebest@gmail.com>
2025-10-25 19:47:57 +0630

    CONTROLLERS

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+  @Get()
+  findAll(@Query('role') role?: 'INTERN | ENGINEER | ADMIN') {
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return {id, ...userUpdate}
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id }
+  }
+
+}

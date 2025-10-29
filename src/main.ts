4764e730129adde657cd1d6e009d83f8cebba81f
yukinoIsMine <dayrotisthebest@gmail.com>
2025-10-29 10:55:02 +0630

    enabling cors for frontend

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001', // Change this to your frontend's actual origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // If you need to send cookies or authorization headers
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
